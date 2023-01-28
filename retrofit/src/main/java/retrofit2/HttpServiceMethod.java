491c4baecb67cb87a27fc17fe2a42d54a68f19d4
nimakarimipour <karimipour.nima@gmail.com>
2023-01-28 07:33:23 +0000

    Annotator result Parallel:False Cache:False

--- retrofit/src/main/java/retrofit2/HttpServiceMethod.java
+++ retrofit/src/main/java/retrofit2/HttpServiceMethod.java
@@ -178,7 +178,7 @@ abstract class HttpServiceMethod<ResponseT, ReturnT> extends ServiceMethod<Retur
       this.callAdapter = callAdapter;
     }
 
-    @Override
+    @Nullable @Override
     protected Object adapt(Call<ResponseT> call, Object[] args) {
       call = callAdapter.adapt(call);
 
@@ -210,7 +210,7 @@ abstract class HttpServiceMethod<ResponseT, ReturnT> extends ServiceMethod<Retur
       this.isNullable = isNullable;
     }
 
-    @Override
+    @Nullable @Override
     protected Object adapt(Call<ResponseT> call, Object[] args) {
       call = callAdapter.adapt(call);
 
