f3e41d1823e56e0a2492be6cfe4a99885a27927f
Nima Karimipour <karimipour.nima@gmail.com>
2024-11-16 12:36:54 -0800

    update

---
+++ annotator-command.sh
@@ -0,0 +1,18 @@
+# Remove any old error_output_<UUID>.txt files
+rm -f nullaway_error_*.txt
+
+# Generate a new UUID for the current error output
+uuid=$(uuidgen)
+
+# Run the command, capture the error output, and display it on stdout
+./gradlew retrofit:compileJava 2>&1 >/dev/null | tee error_temp.txt
+
+# Save the first 4 lines to a unique file
+head -n 4 error_temp.txt > "nullaway_error_${uuid}.txt"
+
+# Clean up the temporary file
+rm error_temp.txt
+
+# print content of nullaway_error_<UUID>.txt
+echo "First NullAway error output:"
+cat "nullaway_error_${uuid}.txt"
---
+++ git_push.sh
@@ -0,0 +1,1 @@
+git add . && git commit -m "resolve" && git push && git rev-parse --verify HEAD | pbcopy && pbpaste
