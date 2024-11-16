8641887c75b4ec99336a86aa6e69ffd7cdace8ca
Nima Karimipour <karimipour.nima@gmail.com>
2024-11-16 13:53:29 -0800

    resolve

--- git_push.sh
+++ git_push.sh
@@ -6,5 +6,6 @@ git push
 if [[ "$(uname)" == "Darwin" ]]; then
     git rev-parse --verify HEAD | pbcopy && pbpaste
 else
-    git rev-parse --verify HEAD | xclip -selection clipboard
+    # just print it on output
+    git rev-parse --verify HEAD
 fi
