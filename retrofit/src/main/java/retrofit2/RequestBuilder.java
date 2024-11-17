2efef4f79f20e6a6eb424ff8f1dfc6df2a47eed3
Nima Karimipour <karimipour.nima@gmail.com>
2024-11-16 16:27:42 -0800

    resolve

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -204,6 +204,9 @@ final class RequestBuilder {
 
   @SuppressWarnings("ConstantConditions") // Only called when isFormEncoded was true.
   void addFormField(String name, String value, boolean encoded) {
+    if(formBuilder == null){
+      throw new RuntimeException("Form Builder is null");
+    }
     if (encoded) {
       formBuilder.addEncoded(name, value);
     } else {
