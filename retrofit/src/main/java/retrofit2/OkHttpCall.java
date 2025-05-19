24cba9826de092d694fd357714e112ba92b12044
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:34:13 -0700

    fix: 8 - DEREFERENCE_NULLABLE - call.cancel(); - dereferenced expression call is @Nullable

--- retrofit/src/main/java/retrofit2/OkHttpCall.java
+++ retrofit/src/main/java/retrofit2/OkHttpCall.java
@@ -141,7 +141,7 @@ final class OkHttpCall<T> implements Call<T> {
     }
 
     if (canceled) {
-      call.cancel();
+      NullabilityUtil.castToNonnull(call, "initialized before 'canceled' check").cancel();
     }
 
     call.enqueue(
