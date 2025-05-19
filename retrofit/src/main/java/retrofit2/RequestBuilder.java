4d91cf2dab4cbd860d08ba805be40e4697caa04d
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:34:36 -0700

    Revert "fix: 10 - DEREFERENCE_NULLABLE - formBuilder.addEncoded(name, value); - dereferenced expression formBuilder is @Nullable"
    This reverts commit 5e0e29d866c924230f150e769e7cbd90a6759ea2.

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -61,7 +61,7 @@ final class RequestBuilder {
 
   private final boolean hasBody;
   private @Nullable MultipartBody.Builder multipartBuilder;
-  @Nullable private FormBody.Builder formBuilder;
+  private @Nullable FormBody.Builder formBuilder;
   private @Nullable RequestBody body;
 
   RequestBuilder(
