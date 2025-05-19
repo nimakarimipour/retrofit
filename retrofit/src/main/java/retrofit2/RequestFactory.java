2cb0f24caf62cf11fd4cacd09d8d02ffb3bd88dc
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:36:00 -0700

    Revert "fix: 11 - DEREFERENCE_NULLABLE - ParameterHandler<?> otherHandler = parameterHandlers[i]; - dereferenced expression parameterHandlers is @Nullable"
    This reverts commit 6883f75cb3f9a79d955ec60f30864fcabc43c903.

--- retrofit/src/main/java/retrofit2/RequestFactory.java
+++ retrofit/src/main/java/retrofit2/RequestFactory.java
@@ -353,6 +353,7 @@ final class RequestFactory {
       return result;
     }
 
+    @Nullable
     private ParameterHandler<?> parseParameterAnnotation(
         int p, Type type, Annotation[] annotations, Annotation annotation) {
       if (annotation instanceof Url) {
@@ -786,20 +787,18 @@ final class RequestFactory {
         validateResolvableType(p, type);
 
         Class<?> tagType = Utils.getRawType(type);
-        if (parameterHandlers != null) {
-          for (int i = p - 1; i >= 0; i--) {
-            ParameterHandler<?> otherHandler = parameterHandlers[i];
-            if (otherHandler instanceof ParameterHandler.Tag
-                && ((ParameterHandler.Tag) otherHandler).cls.equals(tagType)) {
-              throw parameterError(
-                  method,
-                  p,
-                  "@Tag type "
-                      + tagType.getName()
-                      + " is duplicate of parameter #"
-                      + (i + 1)
-                      + " and would always overwrite its value.");
-            }
+        for (int i = p - 1; i >= 0; i--) {
+          ParameterHandler<?> otherHandler = parameterHandlers[i];
+          if (otherHandler instanceof ParameterHandler.Tag
+              && ((ParameterHandler.Tag) otherHandler).cls.equals(tagType)) {
+            throw parameterError(
+                method,
+                p,
+                "@Tag type "
+                    + tagType.getName()
+                    + " is duplicate of parameter #"
+                    + (i + 1)
+                    + " and would always overwrite its value.");
           }
         }
 
