879ff3cc2dcfba7ee2ba19b743d530fc4e2c26f7
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:33:29 -0700

    Revert "fix: 2 - DEREFERENCE_NULLABLE - multipartBuilder.addPart(part); - dereferenced expression multipartBuilder is @Nullable"
    This reverts commit 7ff3245b7b6826f36184c091ea912b9c15857bf1.

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -60,7 +60,7 @@ final class RequestBuilder {
   private @Nullable MediaType contentType;
 
   private final boolean hasBody;
-  @Nullable private MultipartBody.Builder multipartBuilder;
+  private @Nullable MultipartBody.Builder multipartBuilder;
   private @Nullable FormBody.Builder formBuilder;
   private @Nullable RequestBody body;
 
@@ -211,21 +211,13 @@ final class RequestBuilder {
   }
 
   @SuppressWarnings("ConstantConditions") // Only called when isMultipart was true.
-  void addPart(Headers headers, RequestBody body) {
-    if (multipartBuilder == null) {
-      multipartBuilder = new MultipartBody.Builder();
-      multipartBuilder.setType(MultipartBody.FORM);
-    }
+  void addPart(Headers headers, @Nullable RequestBody body) {
     multipartBuilder.addPart(headers, body);
   }
 
   @SuppressWarnings("ConstantConditions") // Only called when isMultipart was true.
   void addPart(MultipartBody.Part part) {
-    if (multipartBuilder != null) {
-      multipartBuilder.addPart(part);
-    } else {
-      throw new IllegalStateException("MultipartBuilder is not initialized.");
-    }
+    multipartBuilder.addPart(part);
   }
 
   void setBody(@Nullable RequestBody body) {
