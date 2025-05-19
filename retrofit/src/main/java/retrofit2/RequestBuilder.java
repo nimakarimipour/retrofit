ffc2913466de5f5ace4cb48d11ba5039291f522b
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:34:29 -0700

    Revert "fix: 9 - DEREFERENCE_NULLABLE - formBuilder.add(name, value); - dereferenced expression formBuilder is @Nullable"
    This reverts commit be238718456f7aa8dca5e65e742d499667ff6ef3.

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -61,7 +61,7 @@ final class RequestBuilder {
 
   private final boolean hasBody;
   private @Nullable MultipartBody.Builder multipartBuilder;
-  @Nullable private FormBody.Builder formBuilder;
+  private @Nullable FormBody.Builder formBuilder;
   private @Nullable RequestBody body;
 
   RequestBuilder(
@@ -203,10 +203,6 @@ final class RequestBuilder {
 
   @SuppressWarnings("ConstantConditions") // Only called when isFormEncoded was true.
   void addFormField(String name, String value, boolean encoded) {
-    if (formBuilder == null) {
-      throw new IllegalStateException(
-          "FormBuilder is null. This method should only be called when form encoding is enabled.");
-    }
     if (encoded) {
       formBuilder.addEncoded(name, value);
     } else {
