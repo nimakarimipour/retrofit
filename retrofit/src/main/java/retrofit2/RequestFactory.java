e91eda154f6d52cecbc6bc9e413c18841c070ab1
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:35:32 -0700

    fix: 3 - DEREFERENCE_NULLABLE - if (!relativeUrlParamNames.contains(name)) { - dereferenced expression relativeUrlParamNames is @Nullable

--- retrofit/src/main/java/retrofit2/RequestFactory.java
+++ retrofit/src/main/java/retrofit2/RequestFactory.java
@@ -825,7 +825,7 @@ final class RequestFactory {
             name);
       }
       // Verify URL replacement name is actually present in the URL path.
-      if (!relativeUrlParamNames.contains(name)) {
+      if (relativeUrlParamNames != null && !relativeUrlParamNames.contains(name)) {
         throw parameterError(method, p, "URL \"%s\" does not contain \"{%s}\".", relativeUrl, name);
       }
     }
