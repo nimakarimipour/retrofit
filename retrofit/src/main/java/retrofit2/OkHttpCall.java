3da78d940596a4c60466a8e5d05baae42453ddf6
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-19 01:34:11 -0700

    Revert "fix: 7 - DEREFERENCE_NULLABLE - call.enqueue( - dereferenced expression call is @Nullable"
    This reverts commit 0cac81c0149e154029ce5ca35cd583a5fc524ac6.

--- retrofit/src/main/java/retrofit2/OkHttpCall.java
+++ retrofit/src/main/java/retrofit2/OkHttpCall.java
@@ -144,42 +144,41 @@ final class OkHttpCall<T> implements Call<T> {
       call.cancel();
     }
 
-    NullabilityUtil.castToNonnull(call, "initialized if null")
-        .enqueue(
-            new okhttp3.Callback() {
-              @Override
-              public void onResponse(okhttp3.Call call, okhttp3.Response rawResponse) {
-                Response<T> response;
-                try {
-                  response = parseResponse(rawResponse);
-                } catch (Throwable e) {
-                  throwIfFatal(e);
-                  callFailure(e);
-                  return;
-                }
+    call.enqueue(
+        new okhttp3.Callback() {
+          @Override
+          public void onResponse(okhttp3.Call call, okhttp3.Response rawResponse) {
+            Response<T> response;
+            try {
+              response = parseResponse(rawResponse);
+            } catch (Throwable e) {
+              throwIfFatal(e);
+              callFailure(e);
+              return;
+            }
 
-                try {
-                  callback.onResponse(OkHttpCall.this, response);
-                } catch (Throwable t) {
-                  throwIfFatal(t);
-                  t.printStackTrace(); // TODO this is not great
-                }
-              }
+            try {
+              callback.onResponse(OkHttpCall.this, response);
+            } catch (Throwable t) {
+              throwIfFatal(t);
+              t.printStackTrace(); // TODO this is not great
+            }
+          }
 
-              @Override
-              public void onFailure(okhttp3.Call call, IOException e) {
-                callFailure(e);
-              }
+          @Override
+          public void onFailure(okhttp3.Call call, IOException e) {
+            callFailure(e);
+          }
 
-              private void callFailure(Throwable e) {
-                try {
-                  callback.onFailure(OkHttpCall.this, e);
-                } catch (Throwable t) {
-                  throwIfFatal(t);
-                  t.printStackTrace(); // TODO this is not great
-                }
-              }
-            });
+          private void callFailure(Throwable e) {
+            try {
+              callback.onFailure(OkHttpCall.this, e);
+            } catch (Throwable t) {
+              throwIfFatal(t);
+              t.printStackTrace(); // TODO this is not great
+            }
+          }
+        });
   }
 
   @Override
