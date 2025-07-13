0729525dde8f790d7856d12d308951963971b4f7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:34:23 -0700

    Revert "fix: 4 - DEREFERENCE_NULLABLE - urlBuilder.addEncodedQueryParameter(name, value); - dereferenced expression urlBuilder is @Nullable"
    This reverts commit 192de5ce30cf2d9fc4c6ec673e2fc9eb1a1e1e2a.

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -15,7 +15,6 @@
  */
 package retrofit2;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import java.util.regex.Pattern;
 import javax.annotation.Nullable;
@@ -184,6 +183,7 @@ final class RequestBuilder {
 
   void addQueryParam(@Nullable String name, @Nullable String value, boolean encoded) {
     if (relativeUrl != null) {
+      // Do a one-time combination of the built relative URL and the base URL.
       urlBuilder = baseUrl.newBuilder(relativeUrl);
       if (urlBuilder == null) {
         throw new IllegalArgumentException(
@@ -193,9 +193,10 @@ final class RequestBuilder {
     }
 
     if (encoded) {
-      Nullability.castToNonnull(urlBuilder, "initialization cannot fail")
-          .addEncodedQueryParameter(name, value);
+      //noinspection ConstantConditions Checked to be non-null by above 'if' block.
+      urlBuilder.addEncodedQueryParameter(name, value);
     } else {
+      //noinspection ConstantConditions Checked to be non-null by above 'if' block.
       urlBuilder.addQueryParameter(name, value);
     }
   }
