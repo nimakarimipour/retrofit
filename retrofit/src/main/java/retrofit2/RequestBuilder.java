141989d2c6c6256780fd1a97c7b163e3d008b14b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:16:01 -0700

    before:1to0 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression input is @Nullable' /home/nima/Developer/nullness-benchmarks/retrofit/retrofit/src/main/java/retrofit2/RequestBuilder.java:135     for (int i = 0, limit = input.length(); i < limit; i += Character.charCount(codePoint)) {

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -131,24 +131,24 @@ final class RequestBuilder {
   }
 
   private static String canonicalizeForPath(@Nullable String input, boolean alreadyEncoded) {
-    int codePoint;
-    for (int i = 0, limit = input.length(); i < limit; i += Character.charCount(codePoint)) {
-      codePoint = input.codePointAt(i);
-      if (codePoint < 0x20
-          || codePoint >= 0x7f
-          || PATH_SEGMENT_ALWAYS_ENCODE_SET.indexOf(codePoint) != -1
-          || (!alreadyEncoded && (codePoint == '/' || codePoint == '%'))) {
-        // Slow path: the character at i requires encoding!
-        Buffer out = new Buffer();
-        out.writeUtf8(input, 0, i);
-        canonicalizeForPath(out, input, i, limit, alreadyEncoded);
-        return out.readUtf8();
+      if (input == null) {
+        throw new NullPointerException("Input must not be null");
       }
+      int codePoint;
+      for (int i = 0, limit = input.length(); i < limit; i += Character.charCount(codePoint)) {
+        codePoint = input.codePointAt(i);
+        if (codePoint < 0x20
+            || codePoint >= 0x7f
+            || PATH_SEGMENT_ALWAYS_ENCODE_SET.indexOf(codePoint) != -1
+            || (!alreadyEncoded && (codePoint == '/' || codePoint == '%'))) {
+          Buffer out = new Buffer();
+          out.writeUtf8(input, 0, i);
+          canonicalizeForPath(out, input, i, limit, alreadyEncoded);
+          return out.readUtf8();
+        }
+      }
+      return input;
     }
-
-    // Fast path: no characters required encoding.
-    return input;
-  }
 
   private static void canonicalizeForPath(
       Buffer out, String input, int pos, int limit, boolean alreadyEncoded) {
