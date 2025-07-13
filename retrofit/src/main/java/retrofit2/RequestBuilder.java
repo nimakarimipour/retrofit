cce17a66d384afdfcd1b24f852a9375ba7eb9fc2
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:33:00 -0700

    Revert "fix: 6 - DEREFERENCE_NULLABLE - multipartBuilder.addPart(headers, body); - dereferenced expression multipartBuilder is @Nullable"
    This reverts commit fd40e240239878a1d4e20028c64342fd7cb6805a.

--- retrofit/src/main/java/retrofit2/RequestBuilder.java
+++ retrofit/src/main/java/retrofit2/RequestBuilder.java
@@ -210,10 +210,9 @@ final class RequestBuilder {
     }
   }
 
+  @SuppressWarnings("ConstantConditions") // Only called when isMultipart was true.
   void addPart(Headers headers, @Nullable RequestBody body) {
-    if (multipartBuilder != null) {
-      multipartBuilder.addPart(headers, body);
-    }
+    multipartBuilder.addPart(headers, body);
   }
 
   @SuppressWarnings("ConstantConditions") // Only called when isMultipart was true.
