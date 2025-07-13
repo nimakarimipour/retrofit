5d90ad559c510a2329c2ed19f941ce9fccc09918
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 15:33:25 -0700

    fix: 11 - DEREFERENCE_NULLABLE - ParameterHandler<?> otherHandler = parameterHandlers[i]; - dereferenced expression parameterHandlers is @Nullable

--- retrofit/src/main/java/retrofit2/RequestFactory.java
+++ retrofit/src/main/java/retrofit2/RequestFactory.java
@@ -787,18 +787,20 @@ final class RequestFactory {
         validateResolvableType(p, type);
 
         Class<?> tagType = Utils.getRawType(type);
-        for (int i = p - 1; i >= 0; i--) {
-          ParameterHandler<?> otherHandler = parameterHandlers[i];
-          if (otherHandler instanceof ParameterHandler.Tag
-              && ((ParameterHandler.Tag) otherHandler).cls.equals(tagType)) {
-            throw parameterError(
-                method,
-                p,
-                "@Tag type "
-                    + tagType.getName()
-                    + " is duplicate of parameter #"
-                    + (i + 1)
-                    + " and would always overwrite its value.");
+        if (parameterHandlers != null) {
+          for (int i = p - 1; i >= 0; i--) {
+            ParameterHandler<?> otherHandler = parameterHandlers[i];
+            if (otherHandler instanceof ParameterHandler.Tag
+                && ((ParameterHandler.Tag) otherHandler).cls.equals(tagType)) {
+              throw parameterError(
+                  method,
+                  p,
+                  "@Tag type "
+                      + tagType.getName()
+                      + " is duplicate of parameter #"
+                      + (i + 1)
+                      + " and would always overwrite its value.");
+            }
           }
         }
 
